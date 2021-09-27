f2d51ad066fd2c1be5fa53a8c0d90b4bdb59f422
HFFHCABeta <89757293+HFFHCABeta@users.noreply.github.com>
2021-09-27 10:44:32 -0700

    Add files via upload

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,5 +3,5 @@ const hamburger = document.getElementById('hamburger');
 const navbar = document.getElementById('navbar');
 
 hamburger.addEventListener('click', () => {
-    navbar.classList.toggle('show');
+    alert("Click")
 });
